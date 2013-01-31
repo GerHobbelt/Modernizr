b6771d9c1a41e85e258672f7f45f8e0331c0c1a1
Ger Hobbelt <ger@hobbelt.com>
2013-02-01 00:46:50 +0100

    updated touchevent detection for IE10 (old code for this got dropped in the latest merge/upgrade);more info at http://blogs.msdn.com/b/ie/archive/2011/10/19/handling-multi-touch-and-mouse-input-in-all-browsers.aspx

--- feature-detects/touchevents.js
+++ feature-detects/touchevents.js
@@ -17,6 +17,11 @@ define(['Modernizr', 'prefixes', 'testStyles'], function( Modernizr, prefixes, t
     var bool;
     if(('ontouchstart' in window) || window.DocumentTouch && document instanceof DocumentTouch) {
       bool = true;
+    } else if (window.navigator.msMaxTouchPoints > 0) {
+      // http://blogs.msdn.com/b/ie/archive/2011/10/19/handling-multi-touch-and-mouse-input-in-all-browsers.aspx      
+      // http://msdn.microsoft.com/en-us/library/ie/hh920754(v=vs.85).aspx
+      // http://blog.stevelydford.com/2012/03/detecting-touch-hardware-in-ie-10/
+      bool = true;
     } else {
       var query = ['@media (',prefixes.join('touch-enabled),('),'heartz',')','{#modernizr{top:9px;position:absolute}}'].join('');
       testStyles(query, function( node ) {
